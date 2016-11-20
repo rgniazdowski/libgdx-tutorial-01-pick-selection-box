d1f38f977ee3f2327e3e845359be6919dda1ed25
Radosław Gniazdowski <7131876+rgniazdowski@users.noreply.github.com>
2016-11-20 17:29:19 +0100

    Updated config in DesktopLauncher

--- desktop/src/com/flexigame/pickselectiondemogdx/desktop/DesktopLauncher.java
+++ desktop/src/com/flexigame/pickselectiondemogdx/desktop/DesktopLauncher.java
@@ -8,7 +8,12 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1024;
-		config.height = 1024;
+		config.height = 768;
+		config.resizable = false;
+		config.allowSoftwareMode = false;
+		config.forceExit = true;
+		config.backgroundFPS = 24;
+		config.fullscreen = false;
 		config.title = "Pick Selection Box Demo - flexigame.com Technical Blog";
 		new LwjglApplication(new MyGdxPickSelectionDemo(), config);
 	}
